1a7fd7c134576292a025e469aa09c3b196b073c7
ananyal <alingineni999@gmail.com>
2018-07-25 11:06:18 -0500

    Cotinuously travels along bottom

--- stacker.py
+++ stacker.py
@@ -17,15 +17,13 @@ class stack():
         i = 0
         pygame.time.set_timer(USEREVENT +1, 800)
         while self.gaming:
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    while i < 8:
-                        sense.set_pixel(i, 7, (0,0,255))
-                        time.sleep(0.1)
-                        sense.set_pixel(i, 7, (0,0,0))
-                        time.sleep(0.05)
-                        i = i+ 1
-                    
+            while i < 8:
+                sense.set_pixel(i, 7, (0,0,255))
+                time.sleep(0.2)
+                sense.set_pixel(i, 7, (0,0,0))
+                time.sleep(0.05)
+                i = i+ 1
+            i = 0               
 
 if __name__ == "__main__":
     try:
